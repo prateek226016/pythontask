62b6af699fb89413c58571706ce8d4373d2cf1ae
prateek226016 <prateek23.84@gmail.com>
2025-08-11 02:14:24 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+a=input('first number: ')
+b=input('second number: ')
+a=float(a)
+b=float(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition =",c)
+print("subtraction =",d)
+print("multiplication =",e)
+print("division =",f)
+
---
+++ task2.py
@@ -0,0 +1,4 @@
+a=input("first name: ")
+b=input("last name: ")
+c="hello "+a+" "+b+ " ! Welcome to the python program"
+print(c)
